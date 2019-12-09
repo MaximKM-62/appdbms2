b6c2ad02d92af2e72567c8e282a4616b5cb7709d
MaximKM-62 <37383153+MaximKM-62@users.noreply.github.com>
2019-12-09 10:29:49 +0200

    Update credentials.py

--- dal/credentials.py
+++ dal/credentials.py
@@ -1,6 +1,9 @@
+import os
 BASE = 'postgresql+psycopg2'
-USERNAME = 'postgres'
-PASSWORD = '2705'
-HOST = 'localhost'
+USERNAME = 'tzxlafwyaqqlzf'
+PASSWORD = '2cd44532fb7672b16eafda82016b90af320812680c97d421e763af28bc15e034'
+HOST = 'ec2-54-75-235-28.eu-west-1.compute.amazonaws.com'
 PORT = '5432'
-DATABASE = 'Student'
+DATABASE = 'd7q78cmhi891kf'
+DATABASE_URI = os.getenv('postgres://tzxlafwyaqqlzf:2cd44532fb7672b16eafda82016b90af320812680c97d421e763af28bc15e034@ec2-54-75-235-28.eu-west-1.compute.amazonaws.com:5432/d7q78cmhi891kf')
+
